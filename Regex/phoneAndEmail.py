42c41b66f6417ac565bfe1e25563c28023dcd74a
Estêvão J. dos Santos <esrevaojs@gmail.com>
2020-07-06 13:04:16 -0300

    Example Begin

---
+++ Regex/phoneAndEmail.py
@@ -0,0 +1,13 @@
+#! python 3
+
+import re, pyperclip
+
+# TODO: Create a regex for phone numbers
+
+# TODO: Create a regex ffor email address
+
+# TODO: Get the text off the clipboard
+
+# TODO: Extract the email/phone from this text
+
+# TODO: Copy the extracted email/phone to the clipboard
